c66a1ddad03f5f154c1f6809efc0d5ec8a6b54ff
BananaBoii <98301106+BananaBoii600@users.noreply.github.com>
2022-04-26 11:14:49 +0530

    everything changed

--- main.py
+++ main.py
@@ -1,69 +1,46 @@
-#Packages
 import random
+import time
 
+def main():
+    print('Epik Dice Roller')
+    player_input = input('Press d to roll dice: ')
+    if player_input.lower() == 'd':
+        dice_roller()
+    elif player_input.lower() != 'd':
+        print("bruh")
+        time.sleep(3)
 
-number = random.randint(1,6)
+def dice_roller():
+    number = random.randint(1, 6)
+    if number == 1:
+        print('You got a 1')
+        print(' ')
+        time.sleep(3)
+        main()
+    elif number == 2:
+        print('You got a 2')
+        print(' ')
+        time.sleep(3)
+        main()
+    elif number == 3:
+        print('You got a 3')
+        print(' ')
+        time.sleep(3)
+        main()
+    elif number == 4:
+        print('You got a 4')
+        print(' ')
+        time.sleep(3)
+        main()
+    elif number == 5:
+        print('You got a 5')
+        print(' ')
+        time.sleep(3)
+        main()
+    elif number == 6:
+        print('You got a 6')
+        print(' ')
+        time.sleep(3)
+        main()
 
-if number == 1:
-    print('''
-                     _____________
-                    |             |
-                    |             |
-                    |      1      |
-                    |             |
-                    |_____________|
-        ''')
-
-elif number == 2:
-    print('''
-                     _____________
-                    |             |
-                    |             |
-                    |      2      |
-                    |             |
-                    |_____________|       
-        ''')
-elif number == 3:
-    print("""
-                     _____________
-                    |             |
-                    |             |
-                    |      3      |
-                    |             |
-                    |_____________|
-        
-        """)
-
-elif number == 4:
-    print("""
-                     _____________
-                    |             |
-                    |             |
-                    |      4      |
-                    |             |
-                    |_____________|
-        
-        """) 
-
-elif number == 5:
-    print("""
-                     _____________
-                    |             |
-                    |             |
-                    |      5      |
-                    |             |
-                    |_____________|
-        """)
-
-elif number == 6:
-    print("""
-                     _____________
-                    |             |
-                    |             |
-                    |      6      |
-                    |             |
-                    |_____________|
-        
-        """) 
-
-input("Enter any key to quit")
+main()
